c1b1ae0411c98aeb98694fc9b1857f62321a13ef
Jubet Padilla <36235518+iamjbpadilla@users.noreply.github.com>
2026-08-02 21:15:23 +0800

    Add QuoteForm back to landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Hero from '../components/sections/Hero';
 import Services from '../components/sections/Services';
 import ServiceAreas from '../components/sections/ServiceAreas';
 import BookingProcess from '../components/sections/BookingProcess';
+import QuoteForm from '../components/sections/QuoteForm';
 import SEO from '../components/common/SEO';
 
 const Home: React.FC = () => {
@@ -12,6 +13,7 @@ const Home: React.FC = () => {
       <PageLayout>
         <Hero />
         <Services />
+        <QuoteForm />
         <ServiceAreas />
         <BookingProcess />
       </PageLayout>
