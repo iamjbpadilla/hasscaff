cb2b56f9dc1e4776e6059ab578113f96e48c45a9
Jubet Padilla <36235518+iamjbpadilla@users.noreply.github.com>
2026-08-02 21:20:36 +0800

    Remove redundant QuoteForm from landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import Hero from '../components/sections/Hero';
 import Services from '../components/sections/Services';
 import ServiceAreas from '../components/sections/ServiceAreas';
 import BookingProcess from '../components/sections/BookingProcess';
-import QuoteForm from '../components/sections/QuoteForm';
 import SEO from '../components/common/SEO';
 
 const Home: React.FC = () => {
@@ -13,7 +12,6 @@ const Home: React.FC = () => {
       <PageLayout>
         <Hero />
         <Services />
-        <QuoteForm />
         <ServiceAreas />
         <BookingProcess />
       </PageLayout>
